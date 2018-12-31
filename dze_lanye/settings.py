07366bb4d87f1442ef804dbaf72c5851664acc19
pythonbrad <fomegnemeudje@outlook.com>
2018-12-31 07:29:40 +0000

    autoescape off

--- dze_lanye/settings.py
+++ dze_lanye/settings.py
@@ -86,8 +86,14 @@ WSGI_APPLICATION = 'dze_lanye.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pythonbrad$default',
+        'USER': 'pythonbrad',
+        'PASSWORD': 'constructor',
+        'HOST': 'pythonbrad.mysql.pythonanywhere-services.com',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
